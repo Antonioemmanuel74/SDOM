2ba2660c823003a21c1bb87f5ba92f2927b8fe98
Emmanuel-Antonio <antonioemmanuel74@gmail.com>
2015-04-15 12:05:25 +0100

    php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- Start Hesder -->
+<?php include 'include/header.php';?>
+<!-- End header -->
+<!--start divder -->
+<div class="divider">
+    
+</div>
+<!-- divider -->
+<!-- begin 1st swection -->
+<div class="container" style=" width: 100%; height:13cm;background-image:url(images/sec1.gif);  background-size: 100% 100%;
+    background-repeat: no-repeat; text-align: left;">
+   <div class="note" >
+     
+        <h1 style="">lorem ipsum lorem ipsum</h1>
+        <p>lorem ipsum  lorem ipsum lorem ipsum lorem ipsum
+        lorem ipsum  lorem ipsum lorem ipsum lorem ipsum
+        lorem ipsum  lorem ipsum lorem ipsum lorem ipsum
+        lorem ipsum  lorem ipsum lorem ipsum lorem ipsum</p>
+        <div class="buton"><a href="#" style="color: white;"><p>Show Me Result</p></a></div>
+    </div>
+ 
+</div>
+<!-- End Of 1st section -->
+
+<!-- Begin 2nd section -->
+<div class="container">
+<div class="row">
+    
+    <div class="col-sm-4">
+        <div class="bar">
+            
+        </div>
+            <h2><a href="#">Solutions</a></h2>
+            <a href="#"><img src="images/solution.gif" width="340cm" height="100cm"></a>
+            <p class="mar">RedBrick Health reinvigorates health and wellness. Our distinctive consumer experience spans high tech and high touch and is driven by powerful business intelligence and deep clinical and behavioral insight. The result is better engagement and stronger returns on everyone's investment in health.
+            </p>
+<p><a href="#">Read More</a></p>
+
+    </div>
+    
+    <div class="col-sm-4">
+       <div class="bar"></div>
+        <a href="#"><h2>Results</h2></a>
+        <div class="spac"></div>
+       <p class="mar">RedBrick Health reinvigorates health and wellness. Our distinctive consumer experience spans high tech and high touch and is driven by powerful business intelligence and deep clinical and behavioral insight. The result is better engagement and stronger returns on everyone's investment in health.
+    </p>
+       <p><a href="#">Read More</a></p>
+    </div>
+    
+    <div class="col-sm-4">
+      <div class="bar"></div>
+        <a href="#"><h2>What's New?</h2></a>
+        <a href="#"><h3>lorem ipsum lorem ipsum <br> lorem ipsum lorem ipsum <br> lorem ipsum <br></h3></a>
+        <div>
+            <p class="mar">RedBrick Health reinvigorates health and wellness. Our distinctive consumer experience spans high tech and high touch and is driven by powerful business intelligence and deep clinical and behavioral insight. The result is better engagement and stronger returns on everyone's investment in health.
+</p>
+            <p><a href="#">Read More</a></p>
+        </div>
+        <a href="#"><h3>lorem ipsum lorem ipsum </h3></a>
+        <div>
+            <p class="mar">RedBrick Health reinvigorates health and wellness. Our distinctive consumer experience spans high tech and high touch and is driven by powerful business intelligence and deep clinical and behavioral insight. The result is better engagement and stronger returns on everyone's investment in health.
+            <p><a href="#">Read More</a></p>
+        </div>
+    </div>
+   
+</div>
+</div>
+<!-- End if 2nd section -->
+    <!-- Start Footer -->
+<?php include 'include/footer.php';?>
+    <!-- End Footer -->
+</html>
