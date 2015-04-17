fa1ca0b60375e31ccda308898611ddc8477a806a
Emmanuel-Antonio <antonioemmanuel74@gmail.com>
2015-04-17 08:28:58 +0100

    laravel_version
    laravel_version

---
+++ resources/views/include/head.blade.php
@@ -0,0 +1,12 @@
+ <style>
+    
+    </style>
+    <meta charset="utf-8">
+    <!-- Set the viewport so this responsive site displays correctly on mobile devices -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SDOM|WEBSITE</title>
+    <!-- Include bootstrap CSS -->
+    <link href="../includes/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../includes/style.css" rel="stylesheet">
+    
+        
