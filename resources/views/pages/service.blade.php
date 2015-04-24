ac1027f4e52a57c9993b9f3a6538aba21bf14d7b
Emmanuel-Antonio <antonioemmanuel74@gmail.com>
2015-04-24 12:12:54 +0100

    update of SODM 24/Apr/2015

---
+++ resources/views/pages/service.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- Start Hesder -->
+    <head>
+	@include('include.head')
+	
+</head>
+<body>
+<!-- Start Header -->
+@include('include.header')
+<!-- End header -->
+
+<div style="height:10cm;" >
+     
+     
+</div>
+
+<!-- End  2nd section -->
+    
+    <!-- Start Footer -->
+@include('include.footer')
+    <!-- End Footer -->
+    <!-- Footer links -->
+@include('include.foot')
+</body>
+
+</html>
